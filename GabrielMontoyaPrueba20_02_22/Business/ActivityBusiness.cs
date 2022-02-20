c698e13403d49d3fc7d8cbb1c434d7c4a7fdae08
gmv94 <gabrieljaime1994@gmail.com>
2022-02-20 13:56:12 -0500

    Se obtienen datos de Activity, Author, Book y CoverPhoto

--- GabrielMontoyaPrueba20_02_22/Business/ActivityBusiness.cs
+++ GabrielMontoyaPrueba20_02_22/Business/ActivityBusiness.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using GabrielMontoyaPrueba20_02_22.Entities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 
 namespace GabrielMontoyaPrueba20_02_22.Business
 {
     public class ActivityBusiness
     {
+        public List<ActivityEntities> DatosCoverPhoto()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://fakerestapi.azurewebsites.net/api/v1/Activities");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                List<ActivityEntities> activities = JsonConvert.DeserializeObject<List<ActivityEntities>>(json);
+                return activities;
+            }
+        }
     }
 }
--- GabrielMontoyaPrueba20_02_22/Business/BookBusiness.cs
+++ GabrielMontoyaPrueba20_02_22/Business/BookBusiness.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using GabrielMontoyaPrueba20_02_22.Entities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 
 namespace GabrielMontoyaPrueba20_02_22.Business
 {
     public class BookBusiness
     {
+        public List<BookEntities> DatosCoverPhoto()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://fakerestapi.azurewebsites.net/api/v1/Books");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                List<BookEntities> books = JsonConvert.DeserializeObject<List<BookEntities>>(json);
+                return books;
+            }
+        }
     }
 }
